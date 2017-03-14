3dbd4d21145e86089b569db8199ce58fc3a4617e
e <1741093834@qq.com>
2017-03-14 20:31:46 +0800

    修改sha256算法

--- tools/UCLSHA_256.h
+++ tools/UCLSHA_256.h
@@ -9,10 +9,11 @@
 #include <cstring>
 using namespace std;
 
+string sha256(const string &str);
+
 class UCLSHA_256{
 public:
     uint32_t rRot(uint32_t val, int pos);
-    string sha_256(string str);
 };
 
 #endif //UCL_CPP_UCLSHA_256_H
