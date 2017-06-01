db17cad5f74fda58e72283975ee750f1218e23a6
wwei25 <1785333472@qq.com>
2017-06-01 19:50:01 +0800

    final version of UCL code v2.0
    修改了test.h和README.md，增加了code部分的测试程序testUCLCode.h和testUCLCode.cpp。

---
+++ testUCLCode.h
@@ -0,0 +1,19 @@
+//
+// Created by Oneway on 2017/6/1.
+//
+
+#ifndef UCL_CPP_TESTUCLCODE_H
+#define UCL_CPP_TESTUCLCODE_H
+
+#include <string>
+
+using std::string;
+
+void showCodeHex(string s);//以16进制形式输出Code
+
+void test_code();//code部分get set方法测试
+
+
+void test_unpack();//解包测试
+
+#endif //UCL_CPP_TESTUCLCODE_H
