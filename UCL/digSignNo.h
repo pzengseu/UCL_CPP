ea35ecf42a4fcb6ea15898b73feeab7c7e5c36a6
freefly0706 <1741093834@qq.com>
2017-07-11 09:14:12 +0800

    添加摘要与签名宏定义头文件

---
+++ UCL/digSignNo.h
@@ -0,0 +1,19 @@
+//
+// Created by fly on 17-7-11.
+//
+
+#ifndef UCL_CPP_DIGSIGNNO_H
+#define UCL_CPP_DIGSIGNNO_H
+
+#define CRC32        1
+#define MD5          2
+#define SHA_256      3
+#define SHA_512      4
+
+#define RSA          1
+#define ECDSA        2
+#define DSA          3
+#define ECC          4
+#define HMAC         5
+
+#endif //UCL_CPP_DIGSIGNNO_H
--- UCL/test.cpp
+++ UCL/test.cpp
@@ -5,16 +5,7 @@
 #include "test.h"
 #include "../property/GenCDPSProperty.h"
 #include "../property/GenCGPSProperty.h"
-#define CRC32        1
-#define MD5          2
-#define SHA_256      3
-#define SHA_512      4
-
-#define RSA          1
-#define ECDSA        2
-#define DSA          3
-#define ECC          4
-#define HMAC         5
+#include "digSignNo.h"
 
 void testCommand()
 {
